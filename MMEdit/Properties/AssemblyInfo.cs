0fa94526f52c8aa01ac44c4bb7ad45751a8deb0a
Nicengi <nicengi@outlook.com>
2020-02-06 14:46:09 +0800

    更新版本号。

--- MMEdit/Properties/AssemblyInfo.cs
+++ MMEdit/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
 [assembly: AssemblyTitle("MMEdit")]
-[assembly: AssemblyDescription("一个 MME 可视化控制器。")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Nicengi")]
 [assembly: AssemblyProduct("MMEdit")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 //可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值
 //通过使用 "*"，如下所示:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
